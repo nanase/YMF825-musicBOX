8397c10e66d96edfb0e519203a6fd882945349ad
Tomona Nanase <nanase@users.noreply.github.com>
2018-08-28 17:20:49 +0900

    未使用の関数を削除

--- sketch/player/ymf825spi.ino
+++ sketch/player/ymf825spi.ino
@@ -19,8 +19,6 @@ void ymf825_write(byte address, byte data) {
   SPI.transfer(address);
   SPI.transfer(data);
   ymf825_unselect();
-
-  // ymf825_wait(address, data);
 }
 
 void ymf825_burstwrite(byte address, byte* data, uint16_t size) {
@@ -49,30 +47,3 @@ void ymf825_reset_hardware() {
   delayMicroseconds(100);
   disable_ic();
 }
-
-/*
-static void ymf825_wait(byte address, byte data) {
-  switch(address) {
-    case 0x02:
-      if (data != 0x00)
-        delayMicroseconds(1000);
-      break;
-    
-    case 0x08:
-      if (data == 0xf6)
-        delayMicroseconds(1000);
-      else if (data != 0x00)
-        delayMicroseconds(21000);
-    
-    case 0x1a:
-      if (data == 0x0a)
-        delayMicroseconds(1000);
-      else if (data == 0x00)
-        delayMicroseconds(30000);
-      break;
-
-    default:
-      return;
-  }
-}
-*/
