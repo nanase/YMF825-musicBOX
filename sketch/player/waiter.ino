99200869c8b84478dc97f13c7673f40f413dafd6
Nanase <nanase@users.noreply.github.com>
2019-01-19 13:11:55 +0900

    電源投入後70分で演奏が乱れる問題を修正

--- sketch/player/waiter.ino
+++ sketch/player/waiter.ino
@@ -1,6 +1,18 @@
 static unsigned long waitGoal = 0;
 
+extern volatile unsigned long timer0_millis;
+extern volatile unsigned long timer0_overflow_count;
+
+static void clearTCNT0() {
+  cli();
+  timer0_millis = 0;
+  timer0_overflow_count = 0;
+  TCNT0 = 0;
+  sei();
+}
+
 void waitBegin() {
+  clearTCNT0();
   waitGoal = micros();
 }
 
