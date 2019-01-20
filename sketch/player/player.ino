c305f0bc5b0561e138fb21b062eac85e86944fac
Nanase <nanase@users.noreply.github.com>
2019-01-20 13:45:05 +0900

    SPIの初期化処理をspi.inoに分離

--- sketch/player/player.ino
+++ sketch/player/player.ino
@@ -9,11 +9,7 @@ void setup() {
   lcdInitialize();
   debugInitialize();
   setupPort();
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setClockDivider(SPI_CLOCK_DIV4);
-  SPI.setDataMode(SPI_MODE0);
-  SPI.begin();
-  PSerial.println("[DEBUG] SPI Begin");
+  spiInitialize();
 
   if (!sdInitialize())
     while (true)
---
+++ sketch/player/spi.ino
@@ -0,0 +1,8 @@
+void spiInitialize() {
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+  SPI.setDataMode(SPI_MODE0);
+  SPI.begin();
+
+  debugInfo("SPI Begin");
+}
