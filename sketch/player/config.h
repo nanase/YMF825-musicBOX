39999cf067830dcdccde8dd4ffe40f6d8b725aba
Nanase <nanase@users.noreply.github.com>
2019-01-20 10:59:49 +0900

    設定項目をconfig.hに分離

---
+++ sketch/player/config.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// -------------------------------------------------------------------------- //
+// Configurations
+
+// Hardware definitions
+
+const byte PIN_SD = 19;
+
+#define PORT_SS PORTC
+#define PORT_SS_DDR_R DDRC
+const byte PORT_SS_DDR_V = B00111111;
+const byte BIT_SS_LCH    = B00001000;
+const byte BIT_SS_RCH    = B00010000;
+const byte BIT_SS_LR     = BIT_SS_LCH | BIT_SS_RCH;
+
+const byte PIN_BT_PLAY = 2;
+const byte PIN_BT_NEXT = 3;
+
+const byte LCD_PIN_R = 9;
+const byte LCD_PIN_G = 6;
+const byte LCD_PIN_B = 5;
+
+const byte LCD_PIN_RS = 8;
+const byte LCD_PIN_E  = 7;
+const byte LCD_PIN_D4 = 4;
+const byte LCD_PIN_D5 = A2;
+const byte LCD_PIN_D6 = A1;
+const byte LCD_PIN_D7 = A0;
+
+// min: 486
+const int32_t SD_BUFFER_SIZE = 1024;
+
+const char VERSION_STRING[] = "ver.2.0b";
--- sketch/player/definitions.h
+++ sketch/player/definitions.h
@@ -1,33 +1,5 @@
 #pragma once
 
-// -------------------------------------------------------------------------- //
-// Configurations
-
-// Hardware definitions
-
-const byte PIN_SD = 19;
-
-#define PORT_SS PORTC
-#define PORT_SS_DDR_R DDRC
-const byte PORT_SS_DDR_V = B00111111;
-const byte BIT_SS_LCH    = B00001000;
-const byte BIT_SS_RCH    = B00010000;
-const byte BIT_SS_LR     = BIT_SS_LCH | BIT_SS_RCH;
-
-const byte PIN_BT_PLAY = 2;
-const byte PIN_BT_NEXT = 3;
-
-const byte LCD_PIN_R = 9;
-const byte LCD_PIN_G = 6;
-const byte LCD_PIN_B = 5;
-
-const byte LCD_PIN_RS = 8;
-const byte LCD_PIN_E  = 7;
-const byte LCD_PIN_D4 = 4;
-const byte LCD_PIN_D5 = A2;
-const byte LCD_PIN_D6 = A1;
-const byte LCD_PIN_D7 = A0;
-
 // Software definitions
 
 const byte WAIT_RESOLUTION = 10;
@@ -39,12 +11,6 @@ const byte YMF825_BOTH_ENABLE = B00000011;
 // Sketch definitions
 
 #define EOF (-1)
-
-// min: 486
-const int32_t SD_BUFFER_SIZE = 1024;
-
-const char VERSION_STRING[] = "ver.2.0b";
-
 #define _USE_PETIT_SERIAL
 
 // -------------------------------------------------------------------------- //
